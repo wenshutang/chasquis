3781de54b515c6fcb6901db17b9df750eb6fb043
wenshutang <wenshutang@gmail.com>
2015-07-04 08:49:09 -0700

    completed portada section harvesting for all 4 sources

--- app/controllers/portada_controller.rb
+++ app/controllers/portada_controller.rb
@@ -9,7 +9,7 @@ class PortadaController < ApplicationController
   def elapsed_time(article)
     delta_time = Time.parse(DateTime.now.to_s) - Time.parse(article.published_at.to_s)
     return "hace más de un día" if delta_time > 3600*24
-    return "#{(delta_time/60)}.round minutos" if delta_time.round < 3600
+    return "#{(delta_time/60).round} minutos" if delta_time.round < 3600
     return "#{(delta_time/3600).round} horas"
 
   end
